9042ddfba3dadcc3e8731e07ef854ad23e594055
Dark Cloud <dc-mud@outlook.com>
2019-08-15 18:37:17 -0400

    Version bump.

--- src/merc.h
+++ src/merc.h
@@ -22,7 +22,7 @@
 
 // We're going to use this to indicate the version of this release which
 // is arbitrary to the person implementing the game.
-#define VERSION "2019.02.27"
+#define VERSION "2019.08.15"
 
 #define args(list) list
 #define DECLARE_DO_FUN(fun)       DO_FUN    fun
